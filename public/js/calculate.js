b803cd0800c2017b3f3797127b9e1bfc16d4be44
Carlos <cdaccarett93@gmail.com>
2017-04-15 02:00:46 -0700

    api call fix, calculation logic

---
+++ public/js/calculate.js
@@ -0,0 +1,12 @@
+function calculate() {
+    var currentGlucose = Document.getElementById('current').value;
+    var targetGlucose = Document.getElementById('target').value;
+    var inSensitivy = Document.getElementById('sens').value;
+    var carbRatio = Document.getElementById('carbRatio').value;
+    
+    console.log(currentGlucose);
+    console.log(targetGlucose);
+    console.log(inSensitivy);
+    console.log(carbRatio);
+    
+}
